2e54cac67ed5e7ef8b48d763e4588213ac4afc5c
s_koha <pdae9pw2@s.okayama-u.ac.jp>
2023-11-28 00:42:41 +0900

    SSLモデル特徴量抽出器

---
+++ abelab_utils/extract_sslfeature.py
@@ -0,0 +1,34 @@
+# https://huggingface.co/rinna/japanese-hubert-base
+# extractor = ExtractSSLFeature()
+# embeds = extractor(wav_path)
+
+import torch
+import numpy as np
+import soundfile as sf
+
+from speechbrain.dataio.preprocess import AudioNormalizer 
+from transformers import HubertModel
+
+class ExtractSSLFeature:
+    def __init__(self):
+        self._device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        
+        self.model = HubertModel.from_pretrained('rinna/japanese-hubert-base')
+        self.model.to(self._device)
+        self.model.eval()
+
+        self.audio_norm = AudioNormalizer()  # for resampling to 16kHz
+
+    def __call__(self, wav_path: str) -> tuple:
+        wav, sr = sf.read(wav_path)
+        # Amp Normalization -1 ~ 1
+        amax = np.amax(np.absolute(wav))
+        wav = wav.astype(np.float32) / amax
+        # Freq Norm
+        wav = self.audio_norm(torch.from_numpy(wav), sr).unsqueeze(0).to(self._device)
+        # Feature Extraction
+        outputs = self.model(wav, output_hidden_states=True)
+
+        outputs = torch.stack(outputs.get('hidden_states')).squeeze()  # [layer(13), frame(any), feature(768)]
+
+        return outputs
